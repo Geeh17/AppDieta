7b6614d86a1d1e0f70fa0a8e9c34b02f8f4b7273
113297903+Geeh17@users.noreply.github.com <113297903+Geeh17@users.noreply.github.com>
2024-09-24 05:08:26 -0300

    Renderização de componentes.

--- mobile/app/step/index.tsx
+++ mobile/app/step/index.tsx
@@ -1,11 +1,40 @@
-import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import { colors } from "../../constants/colors";
 import { Header } from "../../components/header";
 
 export default function Step() {
   return (
-    <View>
-      <Header/>
+    <View style={styles.container}>
+      <Header step="Passo 1" title="Vamos começar" />
+
+      <ScrollView style={styles.content}>
+<Text style={styles.label}>Nome</Text>
+
+</ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  content: {
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+label:{
+fontSize:16,
+color:colors.white,
+fontWeight: 'bold',
+marginBottom:8,
+}
+});
--- mobile/components/header/index.tsx
+++ mobile/components/header/index.tsx
@@ -1,12 +1,36 @@
-import { View, StyleSheet, Pressable, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Pressable,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { colors } from "../../constants/colors";
+import { router } from "expo-router";
 
-export function Header() {
+interface HeadeProps {
+  step: string;
+  title: string;
+}
+
+export function Header({ step, title }: HeadeProps) {
   return (
-    <View style={styles.container}>
-      <Text>Resposta</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Pressable onPress={() => router.back()}>
+            <Feather name="arrow-left" size={24} color="#000" />
+          </Pressable>
+          <Text style={styles.text}>
+            {step} <Feather name="loader" size={24} color="#000" />
+          </Text>
+        </View>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +40,27 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 14,
     borderBottomLeftRadius: 14,
     marginBottom: 14,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight! + 34 : 34,
+  },
+  content: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 34,
+    borderBottomRightRadius: 14,
+    borderBottomLeftRadius: 14,
+  },
+  row: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+  },
+  title: {
+    fontSize: 30,
+
+    fontWeight: "bold",
+    color: colors.background,
   },
 });
