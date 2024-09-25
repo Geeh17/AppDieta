a59d164f5a90af0c81d03ebd8006a6c9c7bfea97
113297903+Geeh17@users.noreply.github.com <113297903+Geeh17@users.noreply.github.com>
2024-09-25 11:41:24 -0300

    Utilização da biblioteca react hook form

--- mobile/app/step/index.tsx
+++ mobile/app/step/index.tsx
@@ -8,6 +8,19 @@ import {
 } from "react-native";
 import { colors } from "../../constants/colors";
 import { Header } from "../../components/header";
+import { Input } from "../../components/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const schema = z.object({
+  name: z.string().min(1, { message: "O nome é obrigatório" }),
+  weight: z.string().min(1, { message: "O peso é obrigatório" }),
+  age: z.string().min(1, { message: "A idade é obrigatória" }),
+  height: z.string().min(1, { message: "A altura é obrigatória" }),
+});
+
+type FormData = z.infer<typeof schema>
 
 export default function Step() {
   return (
@@ -15,9 +28,9 @@ export default function Step() {
       <Header step="Passo 1" title="Vamos começar" />
 
       <ScrollView style={styles.content}>
-<Text style={styles.label}>Nome</Text>
-
-</ScrollView>
+        <Text style={styles.label}>Nome:</Text>
+        <Input />
+      </ScrollView>
     </View>
   );
 }
@@ -31,10 +44,10 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
     paddingRight: 16,
   },
-label:{
-fontSize:16,
-color:colors.white,
-fontWeight: 'bold',
-marginBottom:8,
-}
+  label: {
+    fontSize: 16,
+    color: colors.white,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
 });
